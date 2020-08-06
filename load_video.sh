785c633d520d8ee1ce6d9f22c7a4137ce311766a
Maximiliano Estudies <maxiestudies@gmail.com>
2020-08-06 15:53:12 +0200

    Add correct flags

--- load_video.sh
+++ load_video.sh
@@ -1,1 +1,1 @@
-vlc --no-playlist-autostart --play-and-pause --fullscreen /opt/vc/src/hello_pi/hello_video/test.h264
+vlc --no-video-title --loop --no-playlist-autostart --play-and-pause --fullscreen /opt/vc/src/hello_pi/hello_video/test.h264
